374a7a1c42661964d059ecdf8cb99f23b951f648
jonathanmurthy <jon.aronmurthy@gmail.com>
2018-09-21 12:25:34 -0400

    Add files via upload

---
+++ Kanye analysis.py
@@ -0,0 +1,58 @@
+common_words =['and', 'a', 'an', 'the', 'that', 'this', '', 'of',
+               'in', 'for', 'at', 'it', 'to', 'the,', 'is' ]
+
+def open_file(file_name):
+    data = open(file_name, 'r')
+    string_data = data.read()
+    new_data = string_data.split('\n')
+    words = []
+    for i in new_data:
+        words += i.lower().split(' ')
+    return words 
+
+def remove_common_words(words):
+    for i in words:
+        if i in common_words:
+            words.remove(i)
+    return words
+
+college_dropout = open_file("Kanye West College Dropout Full.txt")
+
+cleaned_data = remove_common_words(remove_common_words(remove_common_words(college_dropout)))
+
+def words_count(words):  
+    my_dict = {}
+    for x in words:
+        if x in my_dict:
+            my_dict[x] += 1
+        else:
+            my_dict[x] =1
+    return my_dict
+        
+def most_common_words(my_dict):
+    values = my_dict.values()
+    best = max(values)
+    words = []
+    for i in my_dict:
+        if my_dict[i] == best:
+            words.append(i)
+    return (words, best)
+
+def words_often(my_dict, min_times):
+    result = []
+    done = False
+    while not done:
+        temp = most_common_words(my_dict)
+        if temp[1] >= min_times:
+            result.append(temp)
+            for i in temp[0]:
+                del(my_dict[i])
+        else:
+            done = True
+    return result 
+
+print(words_often(words_count(cleaned_data), 2))        
+        
+            
+            
+
